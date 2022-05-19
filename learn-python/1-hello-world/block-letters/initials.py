57120a7d1d86a11061240fe562f0f4dfaac08e47
tri <73009093+panji3@users.noreply.github.com>
2022-05-19 23:01:43 +0700

    Create initials.py

---
+++ learn-python/1-hello-world/block-letters/initials.py
@@ -0,0 +1,31 @@
+#using multi-line string
+test1 = """ SSS   L
+S   S  L
+S      L
+ SSS   L
+    S  L
+S   S  L
+ SSS   LLLLL"""
+print(test1)
+
+#Panji Tri
+#Fun Fact : Cool
+
+#Initial
+print("PPPPPPPPPP")
+print("P        P ")
+print("P        P")
+print("PPPPPPPPPP")
+print("P")
+print("P")
+print("P")
+
+
+#second initial
+print("TTTTTTTTTTT")
+print("     T     ")
+print("     T     ")
+print("     T     ")
+print("     T     ")
+print("     T     ")
+print("     T     ")
